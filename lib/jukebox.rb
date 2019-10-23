61293f2ff39887abcfd64de2a4feb4ab5c82a98e
Hiroshi Sipahioglu <hiroshis91@icloud.com>
2019-10-23 22:49:15 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -2,4 +2,5 @@ require_relative "../lib/advanced_jukebox"
 my_songs = {
   "Go Go GO" => '/users/zadams/.atom/code/labs/jukebox-cli/audio/Emerald-Park/01.mp3',
   "LiberTeens" => '/users/zadams/.atom/code/labs/jukebox-cli/audio/Emerald-Park/02.mp3',
+  "Hamburg" =>  '/users/zadams/.atom/code/labs/jukebox-cli/audio/Emerald-Park/03.mp3',
   
